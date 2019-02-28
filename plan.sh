5450a2a8ecc4960da2112c88c922237a30572d36
Duncan Brown <dabrown@syr.edu>
2019-02-28 17:16:57 -0500

    fixed errors in dax creation

--- plan.sh
+++ plan.sh
@@ -8,9 +8,9 @@ pegasus-plan -Dpegasus.workflow.root.uuid=cdf2d39d-a15b-4399-8160-8f02301bd271 \
   -Dpegasus.dir.storage.mapper.replica.file=main.map \
   --conf pegasus.properties \
   --dir `pwd` \
-  --relative-dir ./test_condorio-main_ID0000001
+  --relative-dir ./test_condorio-main_ID0000001 \
   --relative-submit-dir ./test_condorio-main_ID0000001 \
-  --sites local 
-  --cluster label,horizontal
+  --sites local \
+  --cluster label,horizontal \
   --output-site local --cleanup none --verbose  --verbose  --verbose  \
   --deferred --group pegasus --dax testworkflow.dax
